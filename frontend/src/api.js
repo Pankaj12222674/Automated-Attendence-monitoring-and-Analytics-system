24aceeb0cba4e937defc3c0ea1527d44c5496d15
pankaj <pk4801377@gmail.com>
2026-04-14 12:16:38 +0530

    set production backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000", // use Vite env if set
+  baseURL: "https://automated-attendence-monitoring-and.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
